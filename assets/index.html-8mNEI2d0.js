1797fa07539337060d3f2ca4d63711caa2c0a071
gway-tech <gway@gway.kr>
2024-09-12 17:33:33 +0900

    Add files via upload

---
+++ assets/index.html-8mNEI2d0.js
@@ -0,0 +1,27 @@
+import{_ as u}from"./plugin-vue_export-helper-DlAUqK2U.js";import{r as d,c as a,a as o,b as e,f as l,w as r,o as m,e as i}from"./app-DH_u4zeC.js";const s={};function p(g,t){const n=d("RouteLink");return m(),a("div",null,[o(" ## Introduction "),t[15]||(t[15]=e("h2",{id:"소개",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#소개"},[e("span",null,"소개")])],-1)),o(" The server management can be done either by using the traditional interface on the server local desktop or by using the remote web interface. Below are the introductions for both. "),t[16]||(t[16]=e("p",null,"서버 관리는 서버 로컬 데스크탑의 기존 인터페이스를 사용하거나 원격 웹 인터페이스를 사용하여 수행할 수 있습니다. 아래는 두 가지에 대한 소개입니다.",-1)),o(" ## Desktop UI Management Interface "),t[17]||(t[17]=e("h2",{id:"데스크탑-ui-관리-인터페이스",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#데스크탑-ui-관리-인터페이스"},[e("span",null,"데스크탑 UI 관리 인터페이스")])],-1)),o(` - [Main Interface](main.md)
+
+- [Settings](set.md)
+
+- [Image Management](image.md)
+
+- [Computer Management](computer.md)
+
+- [User Management](user.md)
+
+- [Cache Management](cache.md)
+
+- [Dual-server hot backup and load balancing](mutilserver.md)
+
+- [Startup in domain environment](domain.md)
+
+- [Network card pnp](pnp.md)
+
+- [Multi-system boot](mutilimage.md)
+
+- [Update of image and data disk](update.md)
+
+- [Multiserver system image synchronization](sync.md)
+
+- [Batch adds computers](import.md)
+
+- [Recommended server settings](recommand.md) `),e("ul",null,[e("li",null,[e("p",null,[l(n,{to:"/guide/server/main.html"},{default:r(()=>t[0]||(t[0]=[i("메인 메뉴")])),_:1})])]),e("li",null,[e("p",null,[l(n,{to:"/guide/server/set.html"},{default:r(()=>t[1]||(t[1]=[i("설정")])),_:1})])]),e("li",null,[e("p",null,[l(n,{to:"/guide/server/image.html"},{default:r(()=>t[2]||(t[2]=[i("이미지 관리")])),_:1})])]),e("li",null,[e("p",null,[l(n,{to:"/guide/server/computer.html"},{default:r(()=>t[3]||(t[3]=[i("컴퓨터 관리")])),_:1})])]),e("li",null,[e("p",null,[l(n,{to:"/guide/server/user.html"},{default:r(()=>t[4]||(t[4]=[i("사용자 관리")])),_:1})])]),e("li",null,[e("p",null,[l(n,{to:"/guide/server/cache.html"},{default:r(()=>t[5]||(t[5]=[i("캐시 관리")])),_:1})])]),e("li",null,[e("p",null,[l(n,{to:"/guide/server/personaldisk.html"},{default:r(()=>t[6]||(t[6]=[i("개인 디스크")])),_:1})])]),e("li",null,[e("p",null,[l(n,{to:"/guide/server/mutilserver.html"},{default:r(()=>t[7]||(t[7]=[i("이중 서버 핫 백업 및 로드 밸런싱")])),_:1})])]),e("li",null,[e("p",null,[l(n,{to:"/guide/server/domain.html"},{default:r(()=>t[8]||(t[8]=[i("도메인 환경 부팅")])),_:1})])]),e("li",null,[e("p",null,[l(n,{to:"/guide/server/pnp.html"},{default:r(()=>t[9]||(t[9]=[i("네트워크 카드 PNP")])),_:1})])]),e("li",null,[e("p",null,[l(n,{to:"/guide/server/mutilimage.html"},{default:r(()=>t[10]||(t[10]=[i("다중 시스템 부팅")])),_:1})])]),e("li",null,[e("p",null,[l(n,{to:"/guide/server/update.html"},{default:r(()=>t[11]||(t[11]=[i("이미지 및 데이터 디스크 업데이트")])),_:1})])]),e("li",null,[e("p",null,[l(n,{to:"/guide/server/sync.html"},{default:r(()=>t[12]||(t[12]=[i("다중 서버 시스템 이미지 동기화")])),_:1})])]),e("li",null,[e("p",null,[l(n,{to:"/guide/server/import.html"},{default:r(()=>t[13]||(t[13]=[i("일괄적으로 컴퓨터 추가")])),_:1})])]),e("li",null,[e("p",null,[l(n,{to:"/guide/server/recommand.html"},{default:r(()=>t[14]||(t[14]=[i("권장 서버 설정")])),_:1})])])])])}const h=u(s,[["render",p],["__file","index.html.vue"]]),y=JSON.parse('{"path":"/guide/server/","title":"데스크탑 UI","lang":"en-US","frontmatter":{"title":"데스크탑 UI","icon":"fa-solid fa-laptop","index":false,"dir":{"order":1},"description":"소개 서버 관리는 서버 로컬 데스크탑의 기존 인터페이스를 사용하거나 원격 웹 인터페이스를 사용하여 수행할 수 있습니다. 아래는 두 가지에 대한 소개입니다. 데스크탑 UI 관리 인터페이스","head":[["meta",{"property":"og:url","content":"https://vuepress-theme-hope-docs-demo.netlify.app/guide/guide/server/"}],["meta",{"property":"og:site_name","content":"Cloud-i Admin Manual"}],["meta",{"property":"og:title","content":"데스크탑 UI"}],["meta",{"property":"og:description","content":"소개 서버 관리는 서버 로컬 데스크탑의 기존 인터페이스를 사용하거나 원격 웹 인터페이스를 사용하여 수행할 수 있습니다. 아래는 두 가지에 대한 소개입니다. 데스크탑 UI 관리 인터페이스"}],["meta",{"property":"og:type","content":"article"}],["meta",{"property":"og:locale","content":"en-US"}],["meta",{"property":"article:author","content":"Cloud-i Desktop"}],["script",{"type":"application/ld+json"},"{\\"@context\\":\\"https://schema.org\\",\\"@type\\":\\"Article\\",\\"headline\\":\\"데스크탑 UI\\",\\"image\\":[\\"\\"],\\"dateModified\\":null,\\"author\\":[{\\"@type\\":\\"Person\\",\\"name\\":\\"Cloud-i Desktop\\",\\"url\\":\\"https://gway-tech.github.io/guide/\\"}]}"]]},"headers":[{"level":2,"title":"소개","slug":"소개","link":"#소개","children":[]},{"level":2,"title":"데스크탑 UI 관리 인터페이스","slug":"데스크탑-ui-관리-인터페이스","link":"#데스크탑-ui-관리-인터페이스","children":[]}],"git":{"createdTime":null,"updatedTime":null,"contributors":[]},"readingTime":{"minutes":0.41,"words":124},"filePathRelative":"guide/server/README.md","autoDesc":true}');export{h as comp,y as data};
